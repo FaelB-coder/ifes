c746dec7f3a4cef0dac0eb17595e5c6e7c4468fa
Faeldelas <127904171+Faeldelas@users.noreply.github.com>
2023-03-14 20:09:48 -0300

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author 20231tpmi0465
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String nome = "Raphael";
+        double lado = 4.99;
+        double area = lado*lado;
+        System.out.println("O quadrado de lado "+ lado +"tem área" + area);
+        System.out.println("Oi " + nome);
+    }
+    
+}
